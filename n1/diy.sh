2438a6fe6b1343e1dd369d7e33b91966cc7d2ba2
zzzzzzqqq8 <71330272+zzzzzzqqq8@users.noreply.github.com>
2024-11-15 20:17:38 +0800

    Commit: 2024-11-15 20:17:38

--- n1/diy.sh
+++ n1/diy.sh
@@ -5,3 +5,4 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 
 # Add packages
 git clone --single-branch --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+git clone --single-branch --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
